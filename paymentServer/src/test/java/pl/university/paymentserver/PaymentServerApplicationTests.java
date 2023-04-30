fc800f0c9c3134861311ec1318fddac8b96d7b46
gitEugeneL <l.eugene.arch@gmail.com>
2023-04-30 15:24:23 +0200

    initial commit

---
+++ paymentServer/src/test/java/pl/university/paymentserver/PaymentServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.university.paymentserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
